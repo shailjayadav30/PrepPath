ace582bba741353cdb50733b969b0a855ca3ea6c
shailjayadav30 <shailjayadav7275@gmail.com>
2026-08-26 23:52:48 +0530

    feat: update dependencies and implement authentication features
    - Updated package dependencies for better-auth and expo packages.
    - Refactored HomeScreen to display user session information.
    - Added SignIn component for email/password authentication.
    - Added SignUp component for user registration with name, email, and password.
    - Implemented SocialSignIn component for Google authentication.
    - Created auth-client to manage authentication with Better Auth backend.

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,15 @@
+import { createAuthClient } from "better-auth/react";
+import { expoClient } from "@better-auth/expo/client";
+import * as SecureStore from "expo-secure-store";
+
+export const authClient = createAuthClient({
+  baseURL: "http://localhost:8081", // Base URL of your Better Auth backend.
+  plugins: [
+    expoClient({
+      //   scheme: "studyfrontend",
+      //  cookiePrefix: "better-auth",
+      storagePrefix: "studyfrontend",
+      storage: SecureStore,
+    }),
+  ],
+});
